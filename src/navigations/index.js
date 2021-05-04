1ee55aa17dcd7c36cda307223b4b1dd40a05e2f6
Aishanaya01 <aishanayashukla85@gmail.com>
2021-05-04 19:36:58 +0530

    Contact App

---
+++ src/navigations/index.js
@@ -0,0 +1,13 @@
+import React from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {Text} from 'react-native';
+const AppNavContainer=()=>{
+return(
+<NavigationContainer>
+      
+        <Text>hello world</Text>
+     
+    </NavigationContainer>
+);
+}
+export default AppNavContainer;
